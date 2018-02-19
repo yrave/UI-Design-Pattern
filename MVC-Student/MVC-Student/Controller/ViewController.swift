c79e4642afb8f84c332c6ec685aebc834b70c2a4
yrave <yannick@wrave.de>
2018-02-19 18:12:45 +0100

    Added MVC Student

---
+++ MVC-Student/MVC-Student/Controller/ViewController.swift
@@ -0,0 +1,47 @@
+//
+//  ViewController.swift
+//  MVC-Student
+//
+//  Created by Yannick Rave on 19.02.18.
+//  Copyright © 2018 Yannick Rave. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var studentView: StudentView!
+    let student = Student(
+        firstName: "John", lastName: "Doe",
+        grades: [50, 50, 100, 60, 85], studentID: 123456)
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupStudent()
+        studentView.newGradeTextField.delegate = self
+    }
+
+    func setupStudent() {
+        self.studentView.fullNameLabel.text = "\(self.student.firstName) \(self.student.lastName)"
+        self.studentView.studentIDLabel.text = "\(student.studentID)"
+        setupStudentGrades()
+    }
+    
+    func setupStudentGrades() {
+        self.studentView.allGradesLabel.text = student.grades.map({ "\($0)" }).joined(separator: ", ")
+        self.studentView.gradePointAverageLabel.text = "\(student.grades.reduce(0, +) / student.grades.count)"
+    }
+}
+
+extension ViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        guard let grade = textField.text.flatMap({ Int($0) }) else { return true } //TODO: Show error
+        do {
+            try student.add(grade: grade)
+            setupStudentGrades()
+            textField.text = ""
+        } catch { } //TODO: Show error
+        return true
+    }
+}
---
+++ MVC-Student/MVC-Student/View/StudentView.swift
@@ -0,0 +1,24 @@
+//
+//  StudentView.swift
+//  MVC-Student
+//
+//  Created by Yannick Rave on 19.02.18.
+//  Copyright © 2018 Yannick Rave. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class StudentView: UIView {
+    @IBOutlet var fullNameLabel: UILabel!
+    @IBOutlet var gradePointAverageLabel: UILabel!
+    @IBOutlet var allGradesLabel: UILabel!
+    @IBOutlet var studentIDLabel: UILabel!
+    @IBOutlet var newGradeTextField: UITextField!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        self.fullNameLabel.font = UIFont.systemFont(ofSize: 19, weight: .bold)
+    }
+}
