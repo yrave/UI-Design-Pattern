007bdc326767a834d373c611406f46fbcfe827c8
yrave <yannick@wrave.de>
2018-02-26 21:33:02 +0100

    Added MVC Tests

--- MVC-Student/MVC-StudentTests/MVC_StudentTests.swift
+++ MVC-Student/MVC-StudentTests/MVC_StudentTests.swift
@@ -11,26 +11,36 @@ import XCTest
 
 class MVC_StudentTests: XCTestCase {
     
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    let student = Student(firstName: "John", lastName: "Doe", grades: [80, 90], studentID: 3929)
+    
+    func testName() {
+        XCTAssertEqual(student.firstName, "John")
+        XCTAssertEqual(student.lastName, "Doe")
     }
     
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
+    func testGrades() {
+        XCTAssertEqual(student.grades, [80, 90])
     }
     
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testStudentID() {
+        XCTAssertEqual(student.studentID, 3929)
     }
     
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    func testAverage() {
+        XCTAssertEqual(student.average, 85)
     }
     
+    func testNewGrade() {
+        XCTAssertNoThrow(try student.add(grade: 100))
+        XCTAssertEqual(student.average, 90)
+        XCTAssertEqual(student.grades, [80, 90, 100])
+        
+        XCTAssertThrowsError(try student.add(grade: 1000))
+        XCTAssertThrowsError(try student.add(grade: -1000))
+        XCTAssertEqual(student.average, 90)
+        XCTAssertEqual(student.grades, [80, 90, 100])
+        
+        let studentNoGrades = Student(firstName: "", lastName: "", grades: [], studentID: 0)
+        XCTAssertEqual(studentNoGrades.average, 0)
+    }
 }
